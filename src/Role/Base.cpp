ef76c1bb90f98abe3fc2d0e5793a7516d044f401
linsongguo <gls1196@sjtu.edu.cn>
2019-07-26 00:41:01 +0800

    remove Base.h

--- src/Role/Base.cpp
+++
@@ -1,9 +0,0 @@
-#include "Base.h"
-
-namespace Raft {
-  Base::Base(const RaftServerInfo &_info, const RaftServerCluster &_cluster, const Rpc::RaftRpcClient &_rpcClient) {
-    info = std::make_shared<RaftServerInfo>(_info);
-    cluster = std::make_shared<RaftServerCluster>(_cluster);
-    rpcClient = std::make_shared<Raft::Rpc::RaftRpcClient>(_rpcClient);
-  } 
-}
